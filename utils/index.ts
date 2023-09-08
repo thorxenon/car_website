6f6da65419db924fdb856dcc58a114abeb0e8f9a
Pedro Lucas <83101441+thorxenon@users.noreply.github.com>
2023-09-08 09:57:18 -0300

    project run out successfully

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,15 @@
 import { CarProps } from "@/types";
+import { FilterProps } from "@/types";
 
-export async function fetchCars(){
+export async function fetchCars(filters: FilterProps){
+    const { manufacturer, year, model, limit, fuel } = filters;
+
     const headers={
         'X-RapidAPI-Key': 'd5517e10f2mshb5d680971547e09p19e329jsna5c68a8dc277',
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
       }
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3', {
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
         headers: headers, 
     });
 
@@ -44,4 +47,13 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) =>{
     url.searchParams.append('make',make);
 
     return `${url}`;
+}
+
+export const updateSearchParams = (type: string, value: string) =>{
+  const searchParams = new URLSearchParams(window.location.search);
+
+  searchParams.set(type , value);
+        
+  const newPathName = `${window.location.pathname}?${searchParams.toString()}`
+  return newPathName;
 }
